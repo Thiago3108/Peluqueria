ad52f1f58e9fa0922fca8e37df3bc94db38cad07
Thiago3108 <santiago2234508@correo.uis.edu.co>
2025-03-10 21:42:11 -0500

    Se agregaron botones para generar mas interaccion con los clientes, falta editar las opciones de los botones para que se adapten a las necesidades de la empresa.

--- src/config/env.js
+++ src/config/env.js
@@ -7,5 +7,5 @@ export default {
   API_TOKEN: process.env.API_TOKEN,
   BUSINESS_PHONE: process.env.BUSINESS_PHONE,
   API_VERSION: process.env.API_VERSION,
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 3001,
 };
--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -1,11 +1,13 @@
 import whatsappService from './whatsappService.js';
 
 class MessageHandler {
-  async handleIncomingMessage(message) {
+  async handleIncomingMessage(message, senderInfo) {
     if (message?.type === 'text') {
       const incomingMessage = message.text.body.toLowerCase().trim();
+      
       if (this.isGreeting(incomingMessage)) {
-        await this.sendWelcomeMessage(message.from, message.id);
+        await this.sendWelcomeMessage(message.from, message.id, senderInfo);
+        await this.sendWelcomeMenu(message.from);
       }else{
         const response = `Echo: ${message.text.body}`;
         await whatsappService.sendMessage(message.from, response, message.id);
@@ -19,9 +21,30 @@ class MessageHandler {
     return greetings.includes(message);
   }
 
-  async sendWelcomeMessage(to, messageId) {
-    const welcomeMessage = "Hola, Bienvenido a nuestro servicio de atención al cliente. ¿En qué puedo ayudarte?";
+  getSenderName(senderInfo) {
+    return senderInfo.profile?.name || senderInfo.wa_id || '';
+  }
+
+  async sendWelcomeMessage(to, messageId, senderInfo) {
+    const name = this.getSenderName(senderInfo).match(/(\w+)/)?.[1] || "Parce";
+    const welcomeMessage = `¡Hola ${name}!, Bienvenido a nuestro servicio de atención al cliente!. ¿En qué puedo ayudarte?`;
     await whatsappService.sendMessage(to, welcomeMessage, messageId);
+  }
+  async sendWelcomeMenu(to){
+    const menuMessage = `Por favor, selecciona una de las siguientes opciones:`
+    const buttons = [
+      {
+        type: 'reply', reply: { id: 'option_1', title: 'Agendar' }
+      },
+      {
+        type: 'reply', reply: { id: 'option_2', title: 'Ubicación' }
+      },
+      {
+        type: 'reply', reply: { id: 'option_3', title: 'Otra cosa' }
+      }
+    ];
+
+    await whatsappService.sendInteractiveButtons(to, menuMessage, buttons);
   }
 
 }
